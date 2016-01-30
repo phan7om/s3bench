2e9a47b811a4cdee5640cfb4fae293fc2d806964
Captain Jack Sparrow <sd@freematiq.com>
2016-01-30 18:50:27 +0600

    Added get of one entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,9 +23,11 @@ class DefaultController extends Controller
     public function randomAction(Request $request, $start, $end)
     {
         $number = rand($start, $end); // две цифры, чтобы ab не жаловался на разную длину реквестов
+        $record = $this->getDoctrine()->getRepository('AppBundle:BenchData')->findOneByNumber($number);
         return $this->render('AppBundle:default:random.html.twig', [
             'random_number' => $number,
             'memory_usage' => memory_get_usage(),
+            'record' => $record,
         ]);
     }
 
@@ -34,10 +36,20 @@ class DefaultController extends Controller
      */
     public function fillAction(Request $request, $id_from, $id_to)
     {
+        /* @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+
+        $queryBuilder = $em->createQueryBuilder();
+        $queryBuilder->delete('AppBundle:BenchData');
+        $queryBuilder->getQuery()->execute();
+
         for ($i = $id_from; $i <= $id_to; $i++) {
             $record = new BenchData();
             $record->setName(md5(microtime() + rand(0, 4096)));
+            $record->setNumber($i);
+            $em->persist($record);
         }
+        $em->flush();
         return $this->render('AppBundle:default:fill.html.twig', [
             'total' => $id_to - $id_from,
         ]);
